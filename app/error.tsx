57255ae63a3f2bb8a42cbbcb24a0a68bf996665d
kyenideniz <101068045+kyenideniz@users.noreply.github.com>
2024-04-09 13:21:59 +0300

    Update error.tsx

--- app/error.tsx
+++ app/error.tsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
   return ( 
     <div className="h-full flex flex-col space-y-4 items-center justify-center text-muted-foreground">
       <p>
-        Something went wrong
+        Something went wrong.
       </p>
       <Button variant="secondary" asChild>
         <Link href="/">
